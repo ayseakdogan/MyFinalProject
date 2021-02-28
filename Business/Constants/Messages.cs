03a1d6c938c81b4ee83b4e7635dcdb84e0dbd01d
TOSHIBA <TOSHIBA@TSHBA>
2021-02-28 19:45:49 +0300

    Auth ve User alanları, SecuredOperation, IoC, Extensions,  eklendi

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -18,5 +20,13 @@ namespace Business.Constants
         public static string ProductNameExists = "Aynı ürün adından zaten var.";
 
         public static string CategoryLimitExceded = "Kategori limiti aşıldı.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+
+        public static string UserRegistered = "Kayıt oldu";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Parola hatası.";
+        public static string SuccessfulLogin = "Başarılı giriş.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut.";
+        public static string AccessTokenCreated = "Access Token oluşturuldu.";
     }
 }
